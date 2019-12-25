8d7e4684d10fe1b51b59e8a524db4fc95fca3d30
Lakshmiprasad <laksshmiprasadh55@gmail.com>
2019-12-25 18:51:22 +0530

    added resume

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ const app = express()
 const port = process.env.PORT || 3000
 app.use(express.static('public'))
 app.get('/', (req, res) => res.sendFile(__dirname + '/public/html/page1.html'))
+app.get('/data', (req, res) => res.sendFile(__dirname + '/public/html/page1.html'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
