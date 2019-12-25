9c3d09d32b0e2c4df8b686b486939f94860093a8
Lakshmiprasad <laksshmiprasadh55@gmail.com>
2019-12-25 19:33:45 +0530

    modofied1

--- index.js
+++ index.js
@@ -4,4 +4,5 @@ const port = process.env.PORT || 3000
 app.use(express.static('public'))
 app.get('/', (req, res) => res.sendFile(__dirname + '/public/html/page1.html'))
 app.get('/data', (req, res) => res.sendFile(__dirname + '/public/html/page1.html'))
+app.get('/project1', (req, res) => res.sendFile(__dirname + '/public/html/project1.html'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
